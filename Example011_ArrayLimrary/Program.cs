f9d2acdabd650ba36ceb39a1ac6751b46a3c8a00
Ivan <i.klimenko.eng@gmail.com>
2022-08-21 18:25:48 +0400

    added Example011_ArrayLimrary

---
+++ Example011_ArrayLimrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection) //Метод заполнения массива случайными числами
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col) //Метож вывода массива в консоль
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++; 
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
